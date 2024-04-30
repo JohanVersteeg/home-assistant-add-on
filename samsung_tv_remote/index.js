57bb003901dd4e2841cf9dc49d04e843da7b3032
Johan Versteeg <jversteeg@hroffice.nl>
2024-04-30 15:15:05 +0200

    Removed old endpoints

--- samsung_tv_remote/index.js
+++ samsung_tv_remote/index.js
@@ -2,19 +2,6 @@ const express = require('express')
 const app = express()
 const port = 8001
 
-
-var delay = 1000;
-
-app.get('/screen-off', (req, res) => {
-    turnOff();
-  res.send('Screen off');
-})
-
-app.get('/screen-on', (req, res) => {
-    turnOn();
-  res.send('Screen on');
-})
-
 app.get('/send-key/:key', (req, res) => {
     var key = req.params.key;
     key = key.toUpperCase();
@@ -47,69 +34,3 @@ const SamsungRemote = require('samsung-remote');
 const remote = new SamsungRemote({
     ip: '192.168.2.136'
 });
-
-function turnOff() {
-    remote.isAlive((err) => {
-        if (err) {
-            console.error('TV is offline');
-        } else {
-
-            remote.send('KEY_TOOLS', (err) => {
-                if (err) {
-                    console.error(err);
-                } else {
-                    setTimeout(() => {
-                        remote.send('KEY_UP', (err) => {
-                            if (err) {
-                                console.error(err);
-                            } else {
-                                setTimeout(() => {
-                                    remote.send('KEY_ENTER', (err) => {
-                                        if (err) {
-                                            console.error(err);
-                                        }
-                                    });
-                                }, delay);
-                            }
-                        });
-                    }, delay);
-                }
-            });
-        }
-    });
-}
-
-
-
-
-
-
-function turnOn() {
-    remote.isAlive((err) => {
-        if (err) {
-            console.error('TV is offline');
-        } else {
-            remote.send('KEY_RETURN', (err) => {
-                if (err) {
-                    console.error(err);
-                } else {
-                    setTimeout(() => {
-                        remote.send('KEY_RETURN', (err) => {
-                            if (err) {
-                                console.error(err);
-                            } else {
-                                setTimeout(() => {
-                                    remote.send('KEY_RETURN', (err) => {
-                                        if (err) {
-                                            console.error(err);
-                                        }
-                                    });
-                                }, delay);
-                            }
-                        });
-                    }, delay);
-                }
-            });
-        }
-    });
-}
