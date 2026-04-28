fec9ea57693e471c2fa428149b5c6cdf425765e0
Johan Versteeg <johan.versteeg@iwell.eu>
2026-04-28 14:45:36 +0200

    Added chord pro api addon

---
+++ chord-pro-api/run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+OPTIONS="/data/options.json"
+
+if [ -f "$OPTIONS" ]; then
+    export GitlabBaseUrl="$(jq -r '.gitlab_base_url // "https://gitlab.com/api/v4"' "$OPTIONS")"
+
+    i=0
+    while IFS= read -r origin; do
+        export "AllowedOrigins__${i}=${origin}"
+        i=$((i + 1))
+    done < <(jq -r '.allowed_origins[]?' "$OPTIONS")
+fi
+
+exec dotnet AzureFunctions.dll
